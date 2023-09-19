85ab56e2dd919f362b3d10e4632098a0dac6f143
Barshon <barshonweb@gmail.com>
2023-09-19 23:17:41 +0600

    route setup for project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Mainlayout from './components/layout/Mainlayout';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Orders from './components/orders/Orders';
+import Contact from './contact/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Mainlayout></Mainlayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+        loader: ()=> fetch('tshirts.json')
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/orders",
+        element: <Orders></Orders>,
+      },
+      {
+        path: "/contacts",
+        element: <Contact></Contact>
+      }
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
